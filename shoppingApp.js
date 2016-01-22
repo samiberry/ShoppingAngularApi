c2cc7996efc8511d2b4008b864f53f25232f0887
Samantha.Berry <berrysa@mullenmail.com>
2016-01-22 09:00:50 -0700

    working products

--- shoppingApp.js
+++ shoppingApp.js
@@ -1,14 +1,31 @@
-var myApp = angular.module('myApp', [])
-myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) 
+    { 
+    $routeProvider
+    .when('/products', 
+        {
+        templateUrl: 'pages/products.html',
+        controller: 'productController'
+        })
+    
+    .when('/brands', 
+        {
+        templateUrl: 'pages/brands.html',
+        controller: 'brandController'
+        }) 
+    });
+
+myApp.controller('productController', ['$scope', '$http', function ($scope, $http) 
+    {
     $http.get('http://api.shopstyle.com/api/v2/products?pid=uid8225-32718865-93')
     .success(function(result)
              {
-        $scope.planning=result;
         console.log("You are so smart");
-        $scope.names = []
+        $scope.names = [];
                     for(var i = 0; i < result.products.length; i++)
                 {
-        $scope.names.push(result.products[i][16]);
+        $scope.names.push(result.products[i].name);
                     
                 }
     })
@@ -16,39 +33,18 @@ myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
           {
         console.log("You failed, but you are still smart.")
     });
-    $(document).on('click','.navbar-collapse.in',function(e)
+    }]);
+
+myApp.controller('brandController', ['$scope', '$http', function ($scope, $http) 
     {
-    if( $(e.target).is('a') ) 
-        {
-        $(this).collapse('hide');
-        }
+    $http.get('http://api.shopstyle.com/api/v2/brands?pid=uid8225-32718865-93')
+    .success(function(result)
+             {
+        $scope.test = result.brands[0].name;
+        console.log("You are so very smart");
+    })
+    .error(function(data, status)
+          {
+        console.log("You failed, but you are still smart.");
     });
-
-$(window).load(function(){
-$('#myModal').modal('show');
-});
-var ngDemo = angular.module('ngDemo', ['ngRoute']);
-
-
-ngDemo.config(function($routeProvider) 
-    {
-    $routeProvider
-        .when('/', 
-              {
-              templateUrl : 'pages/home.html',
-              controller  : 'mainController'
-			  })
-        .when('/products', 
-              {
-              templateUrl : 'pages/products.html',
-              controller  : ''
-              })
-        .when('/data', 
-              {
-              templateUrl : 'pages/data.html',
-              controller  : ''
-              });
-	});
-
-
-}]);
+    }]);
