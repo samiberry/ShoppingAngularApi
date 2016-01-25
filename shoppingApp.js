e5d12aade037361be89f1d32796fce42d1ff59fa
samiberry <berrysa@mullenmail.com>
2016-01-24 23:29:08 -0700

    Update shoppingApp.js
    finished

--- shoppingApp.js
+++ shoppingApp.js
@@ -3,6 +3,11 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function ($routeProvider) 
     { 
     $routeProvider
+    .when('/',
+        {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
     .when('/products', 
         {
         templateUrl: 'pages/products.html',
@@ -15,23 +20,36 @@ myApp.config(function ($routeProvider)
         controller: 'brandController'
         }) 
     });
+myApp.controller('homeController', [function ()
+    {
+    }]);
 
 myApp.controller('productController', ['$scope', '$http', function ($scope, $http) 
     {
-    $http.get('http://api.shopstyle.com/api/v2/products?pid=uid8225-32718865-93')
-    .success(function(result)
-             {
-        console.log("You are so smart");
-        $scope.productColor = [];
-        $("#sub1").click(function() {
-
-
-    })
-    });
-    .error(function(data, status)
-          {
-        console.log("You failed, but you are still smart.")
-    });
+    $scope.productColor = "";
+    $scope.products = [];
+    $("#sub1").click(function () {
+        $scope.products = [];
+        console.log($scope.productColor);
+        $http.get('http://api.shopstyle.com/api/v2/products?pid=uid8225-32718865-93')
+            .success(function(result) {
+                angular.forEach(result.products , function(value, key) {
+                    console.log(key);
+                    angular.forEach(value.stock , function(value2, key2) {
+                        console.log(value2.color.name);
+                        if(value2.color.name == $scope.productColor){
+                            if($scope.products.indexOf(value.name) == -1) {
+                                $scope.products.push(value.name);
+                            }
+                        }
+                    });
+                });
+                console.log("You are so smart");
+            })
+            .error(function(data, status) {
+                console.log("You failed, but you are still smart.");
+            });
+        });
     }]);
 
 myApp.controller('brandController', ['$scope', '$http', function ($scope, $http) 
@@ -39,11 +57,11 @@ myApp.controller('brandController', ['$scope', '$http', function ($scope, $http)
     $http.get('http://api.shopstyle.com/api/v2/brands?pid=uid8225-32718865-93')
     .success(function(result)
              {
-        $scope.test = result.brands[i].name;
-        console.log("You are so very smart");
+             $scope.results = result;
+            console.log("You are so very smart");
     })
     .error(function(data, status)
           {
         console.log("You failed, but you are still smart.");
-    });
+        });
     }]);
